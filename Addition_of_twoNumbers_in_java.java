c4033a67a12bdcc7992d76ff21d093768473a903
Navneet Bhargava <navneet@xenonstack.com>
2022-07-25 10:45:17 +0530

    this is file for add two numbers in java

---
+++ Addition_of_twoNumbers_in_java.java
@@ -0,0 +1,11 @@
+package addition_of_two.numbers_in_java;
+
+public class Addition_of_twoNumbers_in_java {
+
+    public static void main(String[] args) {
+        int n1 = 225, n2 = 115, sum;
+        sum = n1 + n2;
+        System.out.println("The sum of numbers is: " + sum);
+
+    }
+}
